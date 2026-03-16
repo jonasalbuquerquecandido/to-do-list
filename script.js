774b56583279c17c9313b4e1a4885e90b6b3aab9
Jonas Albuquerque Cândido <jonasalbuquerque.c@gmail.com>
2026-03-15 22:52:47 -0300

    add if in function adicionar

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function adicionar() {
     const tinp = document.getElementById('inpContent').value
 
+    if (tinp.trim() === "") { //sempre usar o trim ao pegar valores do input
+        alert('Escreva um tarefa!')
+        return
+    }
+
     let li = document.createElement("li")
     li.innerHTML = tinp + '<span onclick="deletarTarefa(this)">❌</span>'
 
